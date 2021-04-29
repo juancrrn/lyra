6aa815955d58a11c8dfa2c4d6bdfe4f603f8341f
Juan Carrión <hola@juancrrn.io>
2021-04-29 19:22:47 +0200

    Added utils files

---
+++ src/Domain/BookBank/Utils.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Juancrrn\Lyra\Domain\BookBank;
+
+class Utils
+{
+    
+    
+}
+
+?>
---
+++ src/Domain/GlobalUtils.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Juancrrn\Lyra\Domain;
+
+class GlobalUtils
+{
+    
+    /**
+     * Niveles educativos admitidos
+     */
+    public const EDU_LEVEL_ESO_1    = 'edu_level_eso_1';
+    public const EDU_LEVEL_ESO_2    = 'edu_level_eso_2';
+    public const EDU_LEVEL_ESO_3    = 'edu_level_eso_3';
+    public const EDU_LEVEL_ESO_4    = 'edu_level_eso_4';
+    public const EDU_LEVEL_BACH_1   = 'edu_level_bach_1';
+    public const EDU_LEVEL_BACH_2   = 'edu_level_bach_2';
+    public const EDU_LEVEL_OTHER    = 'edu_level_other';
+
+    public const EDU_LEVELS = array(
+        self::EDU_LEVEL_ESO_1,
+        self::EDU_LEVEL_ESO_2,
+        self::EDU_LEVEL_ESO_3,
+        self::EDU_LEVEL_ESO_4,
+        self::EDU_LEVEL_BACH_1,
+        self::EDU_LEVEL_BACH_2,
+        self::EDU_LEVEL_OTHER
+    );
+}
+
+?>
