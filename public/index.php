7275b182ea470debd0f062d13666ff230c629c06
Juan Carrión <hola@juancrrn.io>
2021-04-30 17:00:10 +0200

    Added route groups to controller index

--- public/index.php
+++ public/index.php
@@ -3,5 +3,53 @@
 require_once __DIR__ . '/../config/init.php';
 
 use Juancrrn\Lyra\Common\App;
+use Juancrrn\Lyra\Common\Controller\AnyoneRouteGroup;
+use Juancrrn\Lyra\Common\Controller\AppManagerRouteGroup;
+use Juancrrn\Lyra\Common\Controller\BookBankManagerRouteGroup;
+use Juancrrn\Lyra\Common\Controller\BookBankVolunteerRouteGroup;
+use Juancrrn\Lyra\Common\Controller\LegalRepRouteGroup;
+use Juancrrn\Lyra\Common\Controller\LoggedInRouteGroup;
+use Juancrrn\Lyra\Common\Controller\StudentRouteGroup;
+
+$controllerInstance = App::getSingleton()->getControllerInstance();
+
+/**
+ * Vistas de usuarios de cualquier tipo (todos)
+ */
+(new AnyoneRouteGroup($controllerInstance))->runAll();
+
+/**
+ * Vistas de usuarios con permisos de gestor de la aplicación (AppManager)
+ */
+(new AppManagerRouteGroup($controllerInstance))->runAll();
+
+/**
+ * Vistas de usuarios con permisos de gestor del banco de libros
+ * (BookBankManager)
+ */
+(new BookBankManagerRouteGroup($controllerInstance))->runAll();
+
+/**
+ * Vistas de usuarios con permisos de voluntario del banco de libros
+ * (BookBankVolunteer)
+ */
+(new BookBankVolunteerRouteGroup($controllerInstance))->runAll();
+
+/**
+ * Vistas de usuarios con permisos de representante legal de estudiantes
+ * (LegalRep)
+ */
+(new LegalRepRouteGroup($controllerInstance))->runAll();
+
+/**
+ * Vistas de usuarios que hayan iniciado sesión
+ */
+(new LoggedInRouteGroup($controllerInstance))->runAll();
+
+/**
+ * Vistas de usuarios con permisos de estudiante (Student) (puede ser receptor
+ * de paquetes del banco de libros)
+ */
+(new StudentRouteGroup($controllerInstance))->runAll();
 
 ?>
