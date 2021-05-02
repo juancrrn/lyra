c0b659b0aefde2cccc10a1442d5f207a1f0e3ec9
Juan Carrión <hola@juancrrn.io>
2021-05-03 00:51:28 +0200

    Added password and token validation

--- src/Common/ValidationUtils.php
+++ src/Common/ValidationUtils.php
@@ -15,14 +15,40 @@ namespace Juancrrn\Lyra\Common;
 class ValidationUtils
 {
 
+	/*
+	 *
+	 * Expresiones regulares
+	 * 
+	 */
+
+	/**
+	 * Rangos UTF-8 estándar
+	 */
+	private const UPPER_EXT_REGEX = 'A-Z\x{00C0}-\x{00D6}\x{00D8}-\x{00DD}';
+	private const LOWER_EXT_REGEX = 'a-z\x{00E0}-\x{00F6}\x{00F8}-\x{00FD}';
+
+	/**
+	 * Formato de token
+	 */
+	private const TOKEN_REGEX = '/^[0-9a-z]{32}$/';
+
+	/**
+	 * Formato de contraseña
+	 */
+	private const PASSWORD_REGEX =
+		'/^' .
+		'(?=.*[' . self::LOWER_EXT_REGEX . '])' . // Al menos, una minúscula (rango extendido)
+		'(?=.*[' . self::UPPER_EXT_REGEX . '])' . // Al menos, una mayúscula (rango extendido)
+		'(?=.*\d)' . // Al menos, un número
+		'.{12,}' . // Cualquier otro caracter hasta completar 12
+		'$/u';
+
     /*
      *
      * Codificación
      * 
      */
 
-    
-	
 	/**
 	 * Comprueba si una cadena está codificada como UTF-8.
 	 * 
@@ -74,5 +100,54 @@ class ValidationUtils
 		} else {
 			return mb_convert_encoding($string, mb_detect_encoding(($string)));
 		}
+	}
+
+	/*
+	 * 
+	 * Formatos propios de la aplicación
+	 * 
+	 */
+
+	/**
+	 * Valida un token de recuperación o activación
+	 * 
+	 * @param mixed $testItem
+	 * 
+	 * @return bool
+	 */
+	public static function validateToken(mixed $testItem): bool
+	{
+		if (! is_string($testItem))
+			return false;
+
+		if (! preg_match(self::TOKEN_REGEX, $testItem))
+			return false;
+
+		return true;
+	}
+
+	/**
+	 * Valida una contraseña.
+	 * 
+	 * Debe tener:
+	 * - Al menos, 12 caracteres.
+	 * - Al menos, una letra mayúscula (rango extendido).
+	 * - Al menos, una letra minúscula (rango extendido).
+	 * - Al menos, un número.
+	 * - No puede contener otro tipo de símbolos.
+	 * 
+	 * @param mixed $testItem
+	 * 
+	 * @return bool
+	 */
+	public static function validatePassword(mixed $testItem): bool
+	{
+		if (! is_string($testItem))
+			return false;
+
+		if (! preg_match(self::PASSWORD_REGEX, $testItem, $unused, PREG_OFFSET_CAPTURE, 0))
+			return false;
+
+		return true;
 	}
 }
