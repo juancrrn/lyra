36b4ac07caa1e477eb933c7fba75f80f1a35ffac
Juan Carrión <hola@juancrrn.io>
2021-05-10 22:17:36 +0200

    Implemented password reset request form and view

---
+++ src/Common/View/Auth/PasswordResetRequestView.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Juancrrn\Lyra\Common\View\Auth;
+
+use Juancrrn\Lyra\Common\App;
+use Juancrrn\Lyra\Common\View\ViewModel;
+use Juancrrn\Lyra\Domain\StaticForm\Auth\LoginForm;
+use Juancrrn\Lyra\Domain\StaticForm\Auth\PasswordResetRequestForm;
+
+/**
+ * Vista de solicitud de restablecimiento de contraseña
+ *
+ * @package lyra
+ * 
+ * @author juancrrn
+ *
+ * @version 0.0.1
+ */
+
+class PasswordResetRequestView extends ViewModel
+{
+    private const VIEW_RESOURCE_FILE    = 'auth/view_password_reset_request';
+    public  const VIEW_NOMBRE           = 'Solicitar restablecimiento de contraseña';
+    public  const VIEW_ID               = 'auth-password-reset-request';
+    public  const VIEW_ROUTE            = '/auth/reset/request/';
+
+    private $form;
+
+    public function __construct()
+    {
+        App::getSingleton()->getSessionManagerInstance()->requireNotLoggedIn();
+
+        $this->name = self::VIEW_NOMBRE;
+        $this->id = self::VIEW_ID;
+
+        $this->form = new PasswordResetRequestForm('/auth/reset/request/'); 
+
+        $this->form->handle();
+        $this->form->initialize();
+    }
+
+    public function processContent(): void
+    {
+        $app = App::getSingleton();
+
+        $filling = array(
+            'form-html' => $this->form->getHtml(),
+            'login-url' => $app->getUrl() . '/auth/login/'
+        );
+
+        $app->getViewManagerInstance()->renderTemplate(self::VIEW_RESOURCE_FILE, $filling);
+    }
+}
---
+++ src/Domain/StaticForm/Auth/PasswordResetRequestForm.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * Formulario de solicitud de restablecimiento de la contraseña
+ * 
+ * @package lyra
+ *
+ * @author juancrrn
+ *
+ * @version 0.0.1
+ */
+
+namespace Juancrrn\Lyra\Domain\StaticForm\Auth;
+
+use Juancrrn\Lyra\Common\App;
+use Juancrrn\Lyra\Domain\Email\EmailUtils;
+use Juancrrn\Lyra\Domain\StaticForm\StaticFormModel;
+use Juancrrn\Lyra\Domain\User\User;
+use Juancrrn\Lyra\Domain\User\UserRepository;
+use RuntimeException;
+
+class PasswordResetRequestForm extends StaticFormModel
+{
+
+    private const FORM_ID = 'form-password-reset-request';
+
+    public function __construct(string $action)
+    {
+        parent::__construct(self::FORM_ID, array('action' => $action));
+    }
+    
+    protected function generateFields(array & $preloadedData = array()): string
+    {
+        $govId = '';
+
+        if (! empty($preloadedData)) {
+            $govId = isset($preloadedData['gov_id']) ? $preloadedData['gov_id'] : $govId;
+        }
+
+        return App::getSingleton()->getViewManagerInstance()->generateViewTemplateRender(
+            'forms/auth/inputs_password_reset_request_form',
+            array(
+                'gov_id' => $govId
+            )
+        );
+    }
+    
+    protected function process(array & $postedData): void
+    {
+        $app = App::getSingleton();
+        $view = $app->getViewManagerInstance();
+
+        $userRepository = new UserRepository($app->getDbConn());
+
+        $govId = isset($postedData['gov_id']) ? $postedData['gov_id'] : null;
+
+        if (empty($govId)) {
+            $view->addErrorMessage('El NIF o NIE no puede estar vacío.');
+        } elseif (! $userRepository->findByGovId($govId)) {
+            $view->addSuccessMessage('Si el NIF o NIE introducido está registrado en nuestros sistemas, recibirás un mensaje de correo electrónico con un mensaje para restablecer tu contraseña.');
+            $view->addSuccessMessage('Por favor, comprueba tu bandeja de correo no deseado o spam. Si no encuentras el mensaje, puedes contactar con nosotros.', '/auth/reset/request');
+        }
+
+        // Si no hay ningún error, continuar.
+        if (! $view->anyErrorMessages()) {
+            $userId = $userRepository->findByGovId($govId);
+
+            $token = $userRepository->generateAndUpdateTokenAndStatusById($userId);
+
+            if ($token) {
+                $user = $userRepository->retrieveById($userId);
+
+                $emailResult = false;
+
+                if ($user->getStatus() == User::STATUS_RESET) {
+                    // Enviar email de restablecimiento.
+                    $emailResult = EmailUtils::sendUserPasswordResetMessage($user);
+                } elseif ($user->getStatus() == User::STATUS_INACTIVE) {
+                    // Enviar email de activcación.
+                    $emailResult = EmailUtils::sendUserActivationMessage($user);
+                } else {
+                    throw new RuntimeException('Invalid user status.');
+                }
+
+                if ($emailResult) {
+                    $view->addSuccessMessage('Si el NIF o NIE introducido está registrado en nuestros sistemas, recibirás un mensaje de correo electrónico con un mensaje para restablecer tu contraseña.');
+                    $view->addSuccessMessage('Por favor, comprueba tu bandeja de correo no deseado o spam. Si no encuentras el mensaje, puedes contactar con nosotros.');
+                }
+            } else {
+                $view->addErrorMessage('Hubo un error al generar el enlace de recuperación. Por favor, vuelve a intentarlo.');
+            }
+        }
+
+        $this->initialize();
+    }
+}
