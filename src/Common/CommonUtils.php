5dc7ad410aaec7453789536b2805689824ec377b
Juan Carrión <hola@juancrrn.io>
2021-05-18 18:53:00 +0200

    Added human date representation constants

--- src/Common/CommonUtils.php
+++ src/Common/CommonUtils.php
@@ -24,4 +24,16 @@ class CommonUtils
      * Formato estándar de tipo de dato DATE de MySQL.
      */
     public const MYSQL_DATE_FORMAT = 'Y-m-d';
+    
+    /**
+     * Formato estándar de tipo de dato DATETIME para legibilidad humana.
+     */
+    public const HUMAN_DATETIME_FORMAT = 'j \d\e F \d\e\l Y \a \l\a\s H:i';
+    public const HUMAN_DATETIME_FORMAT_STRF = '%e de %B de %Y a las %H:%M';
+    
+    /**
+     * Formato estándar de tipo de dato DATE para legibilidad humana.
+     */
+    public const HUMAN_DATE_FORMAT = 'j \d\e F \d\e\l Y';
+    public const HUMAN_DATE_FORMAT_STRF = '%e de %B de %Y';
 }
